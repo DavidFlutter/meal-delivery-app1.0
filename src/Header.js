1789b420c7faf62f29acb240cf36330304e3426a
DavidFlutter <david.haastrup@stu.cu.edu.ng>
2024-01-22 10:02:43 +0000

    Header structure and styles added

---
+++ src/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header>
+        <h1>Medical center meal delivery</h1>
+        <section className="buttons">
+            <button className="cart-btn">
+                Cart
+            </button>
+        </section>
+    </header>
+  )
+}
+
+export default Header
